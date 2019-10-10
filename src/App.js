7786373c671c8a11e202b9b0ceed1ec55fe6ae23
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-10 09:41:09 -0600

    setup componentDidMount on App to get session and assign it to Redux State

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import "./App.css";
 import routes from "./routes";
 import { connect } from "react-redux";
-import { setUser } from "react-redux";
+import { setUser } from "./ducks/reducer";
 import axios from "axios";
 
 export class App extends React.Component {
-  componentDidMount() {}
-  getSession = () => {};
+  componentDidMount() {
+    this.getSession();
+  }
+  getSession = () => {
+    try {
+      axios.get(`/auth/session`).then(res => {
+        this.props.setUser({
+          username: res.data.user.username,
+          userId: res.data.user.user_id
+        });
+      });
+    } catch (err) {
+      console.log(err, "can't get session");
+    }
+  };
   render() {
     return <div>{routes}</div>;
   }
 }
 
-export default App;
+export default connect(
+  null,
+  { setUser }
+)(App);
